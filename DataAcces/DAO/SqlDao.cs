2c5a38d0bbfe0f00084ea254fa39971b965a41ec
charlie <clopezde@ucenfotec.ac.cr>
2025-06-25 19:41:13 -0600

    inclusion tabla usuario y pelicula

--- DataAcces/DAO/SqlDao.cs
+++ DataAcces/DAO/SqlDao.cs
@@ -1,50 +1,84 @@
-﻿using System;
+﻿using DataAccess.DAO;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAcces.DAO
+namespace DataAccess.DAOs
 {
+    /*
+     * Clase u objeto que se encarga de la comunicación con la base de datos.
+     * Solo ejecuta Store Procedures
+     * 
+     * Esta clse implementa el Patron del Singleton para asegurar
+     * la existencia de una unica instancia de este objeto
+     */
     public class SqlDao
     {
-        //paso 1
+        //Paso 1: Crear una instancia privada de la misma clase
         private static SqlDao _instance;
 
         private string _connectionString;
-        
-        //paso 2
+
+        //Paso 2: Redefinir el constructor default y convertirlo en privado
         private SqlDao()
         {
-            // Initialize the connection string here
-            _connectionString = string.Empty; 
+            _connectionString = @"Data Source=srv-sqldatabase-aaguero.database.windows.net;Initial Catalog=cenfocinemas-db;Persist Security Info=True;User ID=sysman;Password=Cenfotec123!;Trust Server Certificate=True";
         }
 
-        //paso 3
-        public SqlDao GetInstance()
+        //Paso 3: Definir el metodo que expone la instancia
+
+        public static SqlDao GetInstance()
         {
+            //Si la instancia es nula, crear una nueva instancia
             if (_instance == null)
             {
                 _instance = new SqlDao();
             }
+            //Retornar la instancia
             return _instance;
         }
 
-        //ejecucion sp sin retorno
-        public void ExecuteProcedure(SqlOperation operation) { 
+        //Metodo para la ejecución de Store Procedures sin retorno
+        public void ExecuteProcedure(SqlOperation sqlOperation)
+        {
+            //Conectarse a la base de datos
+            //Ejecutar el Store Procedure
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand(sqlOperation.ProcedureName, conn)
+                {
+                    CommandType = System.Data.CommandType.StoredProcedure
+                })
+
+                {
+                    //Set de los parametros
+                    foreach (var param in sqlOperation.Parameters)
+                    {
+                        command.Parameters.Add(param);
+                    }
+                    //Ejectura el SP
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                }
+
+            }
         }
 
-
-        //metodosp retorno data
-        public List<Dictionary<string, object>> ExecuteQuery(SqlOperation operation)
+        //Metodo para la ejecución de Store Procedures con retorno
+        public List<Dictionary<string, object>> ExecuteProcedureWithReturn(SqlOperation operation)
         {
+            //Conectarse a la base de datos
+            //Ejecutar el Store Procedure
+            //Capturar el resultado
+            //Convertir el resultado en DTOs
+
             var list = new List<Dictionary<string, object>>();
+
             return list;
         }
-
-
-
-
-
     }
 }
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public ActionResult Create(UserController user)
+        {
+            try
+            {
+                var um = new UserManager();
+                um.Create(user);
+                return Ok(user);
+
+            }
+        }
+    }
+}
